8e43b87375d56d5b29b656a43c888b4fe119bc4f
Roberto Junior <robertojr-13-@hotmail.com>
2020-07-13 18:36:38 -0300

    Add Create-Account Page

---
+++ src/pages/SignUp/index.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { FiArrowLeft, FiMail, FiLock, FiUser } from 'react-icons/fi';
+import { Container, Background, Content } from './styles';
+import logoImg from '../../assets/logo.svg';
+import Button from '../../components/Button';
+import Input from '../../components/Input';
+
+const SignUp: React.FC = () => {
+  return (
+    <Container>
+      <Background />
+
+      <Content>
+        <img src={logoImg} alt="Gobarber" />
+        <form>
+          <h1>Faça seu Cadastro</h1>
+          <Input icon={FiUser} name="name" placeholder="Nome" />
+          <Input icon={FiMail} name="email" placeholder="E-mail" />
+          <Input
+            icon={FiLock}
+            name="password"
+            type="password"
+            placeholder="Senha"
+          />
+          <Button type="submit">Cadastrar</Button>
+        </form>
+        <a href="createAccount">
+          <FiArrowLeft />
+          Voltar para Logon
+        </a>
+      </Content>
+    </Container>
+  );
+};
+
+export default SignUp;
