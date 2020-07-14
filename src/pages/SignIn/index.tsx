4e4f348d3cd316bfdc8ba1fc72dd3e6c28be0f7c
Roberto Junior <robertojr-13-@hotmail.com>
2020-07-13 23:29:11 -0300

    Add Form Configs

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,17 +1,21 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
+import { Form } from '@unform/web';
 import { Container, Background, Content } from './styles';
 import logo from '../../assets/logo.svg';
 import Button from '../../components/Button';
 import Input from '../../components/Input';
 
 const SignIn: React.FC = () => {
+  function handleSubmit(data: Object): void {
+    console.log(data);
+  }
   return (
     <Container>
       <Content>
         <img src={logo} alt="Gobarber" />
-        <form>
+        <Form onSubmit={handleSubmit}>
           <h1>Faça seu Logon</h1>
           <Input icon={FiMail} name="email" placeholder="E-mail" />
           <Input
@@ -22,7 +26,7 @@ const SignIn: React.FC = () => {
           />
           <Button type="submit">Entrar</Button>
           <a href="forgot">Esqueci Minha Senha</a>
-        </form>
+        </Form>
         <Link to="/signUp">
           <FiLogIn />
           Criar Conta
--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,19 +1,23 @@
 import React from 'react';
 import { FiArrowLeft, FiMail, FiLock, FiUser } from 'react-icons/fi';
 import { Link } from 'react-router-dom';
+import { Form } from '@unform/web';
 import { Container, Background, Content } from './styles';
 import logoImg from '../../assets/logo.svg';
 import Button from '../../components/Button';
 import Input from '../../components/Input';
 
 const SignUp: React.FC = () => {
+  function handleSubmit(data: Object): void {
+    console.log(data);
+  }
   return (
     <Container>
       <Background />
 
       <Content>
         <img src={logoImg} alt="Gobarber" />
-        <form>
+        <Form onSubmit={handleSubmit}>
           <h1>Faça seu Cadastro</h1>
           <Input icon={FiUser} name="name" placeholder="Nome" />
           <Input icon={FiMail} name="email" placeholder="E-mail" />
@@ -24,7 +28,7 @@ const SignUp: React.FC = () => {
             placeholder="Senha"
           />
           <Button type="submit">Cadastrar</Button>
-        </form>
+        </Form>
         <Link to="/">
           <FiArrowLeft />
           Voltar para Logon
