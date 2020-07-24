746e076df9e7222a437b28ba17535699875f2503
Roberto Junior <robertojr-13-@hotmail.com>
2020-07-24 18:39:06 -0300

    Add Barber-Schedule section

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,6 +1,15 @@
 import React from 'react';
-import { FiPower } from 'react-icons/fi';
-import { Container, Header, HeaderContent, Profile } from './styles';
+import { FiPower, FiClock } from 'react-icons/fi';
+import {
+  Container,
+  Header,
+  HeaderContent,
+  Profile,
+  Content,
+  Schedule,
+  Calendar,
+  NextAppointments,
+} from './styles';
 import logoImg from '../../assets/logo.svg';
 import { useAuth } from '../../hooks/auth';
 
@@ -23,6 +32,28 @@ const Dashboard: React.FC = () => {
           </button>
         </HeaderContent>
       </Header>
+      <Content>
+        <Schedule>
+          <h1>Horários Agendados</h1>
+          <p>
+            <span>Hoje</span>
+            <span>Dia 05</span>
+            <span>Segunda-feira</span>
+          </p>
+          <NextAppointments>
+            <strong>Atendimento a seguir</strong>
+            <div>
+              <img src={user.avatar_url} alt="Usuario" />
+              <strong>Roberto</strong>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+            </div>
+          </NextAppointments>
+        </Schedule>
+        <Calendar />
+      </Content>
     </Container>
   );
 };
