150a3970cf6244d46eff599f1b7bac6472608bcf
robertokbr <robertojuniordev@gmail.com>
2021-03-24 21:03:28 -0300

    Update utils

--- src/utils/getValidationErrors.ts
+++ src/utils/getValidationErrors.ts
@@ -6,8 +6,10 @@ interface Errors {
 
 export default function getValidationErrors(err: ValidationError): Errors {
   const validationErrors: Errors = {};
+
   err.inner.forEach(error => {
     validationErrors[error.path] = error.message;
   });
+
   return validationErrors;
 }
