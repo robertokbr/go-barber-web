258112d856f22ad8980afe203dc1afd8289fb5db
robertokbr <robertojuniordev@gmail.com>
2021-03-26 17:25:55 -0300

    Update Routes

--- src/Routes/index.tsx
+++ src/Routes/index.tsx
@@ -6,14 +6,14 @@ import Dashboard from '../pages/Dashboard';
 import Route from './Route';
 import ForgotPassword from '../pages/ForgotPassword';
 import ResetPassword from '../pages/ResetPassword';
-import ProfilePage from '../pages/ProfilePage';
+import AlterProfile from '../pages/AlterProfile';
 
 const Routes: React.FC = () => (
   <Switch>
     <Route component={SignIn} path="/" exact />
     <Route component={SignUp} path="/signup" />
     <Route component={Dashboard} path="/dashboard" isPrivate />
-    <Route component={ProfilePage} path="/profile" isPrivate />
+    <Route component={AlterProfile} path="/profile" isPrivate />
     <Route component={ForgotPassword} path="/forgot_password" />
     <Route component={ResetPassword} path="/reset_password" />
   </Switch>
